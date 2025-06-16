11e672ce0db25fb4369c535431bae7ee764e7d12
ranesh <raneshkapoor@ymail.com>
2025-06-16 20:19:42 +0530

    Added kafka practice code

---
+++ src/main/java/org/kafkapractice/Main.java
@@ -0,0 +1,10 @@
+package org.kafkapractice;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World !");
+        System.out.println("Kafka Practice");
+
+    }
+}
