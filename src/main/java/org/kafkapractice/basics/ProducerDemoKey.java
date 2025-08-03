d41f9d071c87c73b00abb4173c095fb61db2d197
ranesh <raneshkapoor@ymail.com>
2025-08-03 23:02:56 +0530

    Added Producer Classes

---
+++ src/main/java/org/kafkapractice/basics/ProducerDemoKey.java
@@ -0,0 +1,46 @@
+package org.kafkapractice.basics;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.Properties;
+
+public class ProducerDemoKey {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKey.class);
+
+    public static void main(String[] args) {
+
+        log.info("Starting Producer");
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", "abcd1234", "Hello World!");
+
+        log.info("Sending message : {}", producerRecord.value());
+
+        producer.send(producerRecord, (recordMetadata, e) -> {
+            if (Objects.isNull(e)) {
+                log.info("Received Metadata :- Partition : {} Timestamp : {}", recordMetadata.partition(), recordMetadata.timestamp());
+            } else {
+                log.error("Error while sending message. Error : {}", e.getMessage());
+            }
+        });
+
+        producer.flush();
+        producer.close();
+
+        log.info("Message Sent. Producer Closed.");
+
+    }
+
+}
